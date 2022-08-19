04588afd9321b8e4b37a8ac05fc6ed434782583c
iman sharifi <friduby@gmail.com>
2022-08-19 16:09:02 +0430

    protecting routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
+import { useState } from "react";
+import Home from "./views/Home";
+import ProtectedRoute from "./tools/ProtectedRoute";
 
 function App() {
-    return <div className="App"></div>;
+    const [user, setUser] = useState(null);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute user={user} />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
