3b5f7a014b9164579a74ed23030f9459c4b0f639
iman sharifi <friduby@gmail.com>
2022-08-26 02:22:57 +0430

    buttons for free and pro added, no functionality

--- src/views/Home/Main/Main.jsx
+++ src/views/Home/Main/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./main.css";
+import ProFreeButtons from "./components/ProFreeButtons";
 
 const Main = () => {
-    return <div>Main</div>;
+    return (
+        <div className="content main">
+            <ProFreeButtons />
+            list of items
+        </div>
+    );
 };
 
 export default Main;
---
+++ src/views/Home/Main/components/ProFreeButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./proFreeButtons.css";
+
+const ProFreeButtons = () => {
+    return (
+        <div className="content proFreeButtons">
+            <button className="proFreeButtons selected">pro</button>
+            <button className="proFreeButtons">free</button>
+        </div>
+    );
+};
+
+export default ProFreeButtons;
