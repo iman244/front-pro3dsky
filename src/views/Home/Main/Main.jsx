bde71ed7c0f5c21d4b9860414aa3d4dac0afd531
Iman Sharifi <imansharifi244@gmail.com>
2022-10-25 02:34:50 +0330

    make sidebar list true

--- src/views/Home/Main/Main.jsx
+++ src/views/Home/Main/Main.jsx
@@ -9,77 +9,81 @@ import ReactLoading from "react-loading";
 import Error403 from "../../../components/Error403/Error403";
 
 const Main = () => {
-  const {
-    isLoading,
-    isError,
-    error,
-    data,
-    setPage,
-    itemsPerPage,
-    DesignsCount,
-  } = useContext(HomeContext);
+    const {
+        isLoading,
+        isError,
+        error,
+        data,
+        page,
+        setPage,
+        itemsPerPage,
+        DesignsCount,
+    } = useContext(HomeContext);
 
-  return (
-    <div className="content main">
-      <ProFreeButtons />
-      <div className="cardList wrapper">
-        {isLoading ? (
-          <ReactLoading
-            type={"bars"}
-            color={"gray"}
-            height={"fit-content"}
-            width={"200px"}
-          />
-        ) : isError ? (
-          <div>Error: {error.message}</div>
-        ) : (
-          <>
-            <div className="grid-container">
-              {data && data.statusCode === 403 ? (
-                <Error403 />
-              ) : data.totalDesigns ? (
-                data.designs.map((card) => {
-                  return (
-                    <div className="card" key={card._id}>
-                      <Card
-                        id={card._id}
-                        src={card.keyList}
-                        desc={card.name}
-                        isPremium={card.isPremium}
-                      />
-                    </div>
-                  );
-                })
-              ) : (
-                <p>there is no item to show!</p>
-              )}
+    return (
+        <div className="content main">
+            <ProFreeButtons />
+            <div className="cardList wrapper">
+                {isLoading ? (
+                    <ReactLoading
+                        type={"bars"}
+                        color={"gray"}
+                        height={"fit-content"}
+                        width={"200px"}
+                    />
+                ) : isError ? (
+                    <div>Error: {error.message}</div>
+                ) : (
+                    <>
+                        <div className="grid-container">
+                            {data && data.statusCode === 403 ? (
+                                <Error403 />
+                            ) : data.totalDesigns ? (
+                                data.designs.map((card) => {
+                                    return (
+                                        <div className="card" key={card._id}>
+                                            <Card
+                                                id={card._id}
+                                                src={card.keyList}
+                                                desc={card.name}
+                                                isPremium={card.isPremium}
+                                            />
+                                        </div>
+                                    );
+                                })
+                            ) : (
+                                <p>there is no item to show!</p>
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
-          </>
-        )}
-      </div>
-      <div className="bottom">
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel="forward"
-          onPageChange={(event) => setPage(event.selected + 1)}
-          pageRangeDisplayed={3}
-          pageCount={Math.ceil(DesignsCount / itemsPerPage)}
-          previousLabel="backward"
-          renderOnZeroPageCount={null}
-          pageClassName="page"
-          pageLinkClassName="pagelink"
-          previousClassName="previouse"
-          previousLinkClassName="previouselink"
-          nextClassName="next"
-          nextLinkClassName="nextlink"
-          breakClassName="break"
-          breakLinkClassName="breaklink"
-          containerClassName="container"
-          activeClassName="active"
-        />
-      </div>
-    </div>
-  );
+            <div className="bottom">
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="forward"
+                    onPageChange={(event) => {
+                        setPage(event.selected + 1);
+                    }}
+                    forcePage={page - 1}
+                    pageRangeDisplayed={3}
+                    pageCount={Math.ceil(DesignsCount / itemsPerPage)}
+                    previousLabel="backward"
+                    renderOnZeroPageCount={null}
+                    pageClassName="page"
+                    pageLinkClassName="pagelink"
+                    previousClassName="previouse"
+                    previousLinkClassName="previouselink"
+                    nextClassName="next"
+                    nextLinkClassName="nextlink"
+                    breakClassName="break"
+                    breakLinkClassName="breaklink"
+                    containerClassName="container"
+                    activeClassName="active"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Main;
